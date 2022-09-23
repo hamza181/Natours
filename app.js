0ca9a6721de456b5da5b9142a569a42c37ed3cbd
Ameer Hamza <ameer.hamza1811@gmail.com>
2022-09-24 01:26:50 +0500

    make get and post api and save in file

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+app.use(express.json());
+
+// app.get('/', (req, res)=>{
+//     // res.status(200).send('Hello from server')
+//     res.status(200).json({message: 'Hello'})
+// })
+
+// app.post('/', (req, res)=>{
+//     res.send('You can post')
+// })
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    success: 'success',
+    result: tours.length,
+    data: { tours },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`app running on port ${port}`);
+});
