83d71611a47563151a15df14f18c7a16d5a8151a
Ameer Hamza <ameer.hamza1811@gmail.com>
2022-10-25 02:41:16 +0500

    apply pagination in get api

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -40,38 +40,52 @@ exports.getAllTours = async (req, res) => {
     //   .where('difficulty')
     //   .equals('easy');
 
-    const queryObj = {...req.query}
+    const queryObj = { ...req.query };
 
     // 1) Filtering
-    const excludedFields = ['page', 'sort', 'limit', 'fields']
-    excludedFields.forEach(el=>delete queryObj[el])
-    
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
     // 2) Advance Filtering
-    let queryStr = JSON.stringify(queryObj)
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match=>`$${match}`)
-    console.log(JSON.parse(queryStr))
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
     // { difficulty: 'easy', duration: { $gte: 5 } }
     // { difficulty: 'easy', duration: { gte: '5' } }
-    
+
     let query = Tour.find(JSON.parse(queryStr));
-    
+
     // 3) Sorting
     if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ')
-      query = query.sort(sortBy)
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
     } else {
-      query = query.sort('-createdAt')
+      query = query.sort('-createdAt');
     }
 
     // 4) Fields limiting
     if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ')
-      query = query.select(fields)
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
     } else {
-      query = query.select('')
+      query = query.select('');
     }
 
-    const tours = await query
+    // 5) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    // page=2&limit=10     1-10, page 1,  11-20, page 2
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
+    const tours = await query;
+    // query.sort().select().skip().limit()
 
     res.status(200).json({
       success: 'success',
